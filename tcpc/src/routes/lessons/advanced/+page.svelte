ab20b6c6ed267418891342c74e2ab4bf294c1745
richardz403 <116985684+richardz403@users.noreply.github.com>
2023-09-04 17:55:09 -0700

    more stuff

---
+++ tcpc/src/routes/lessons/advanced/+page.svelte
@@ -0,0 +1,37 @@
+<h1 class = "text-center font-bold text-4xl text-white drop-shadow-lg">
+    Advanced
+</h1>
+
+<p class = "m-5 sm:mr-40 sm:ml-40 text-center font-semibold text-white text-lg sm:text-2xl"> 
+    For more experienced students who are already comfortable and experienced with CP and have done contests before
+    We start off right away with more complex topics and problems
+    <br>
+    Goal: USACO Silver level -> Gold/Platinum
+
+</p>
+
+<div class = "text-center">
+    <a class = "drop-shadow-md link text-center font-extrabold text-white text-3xl hover:text-cyan-400 hover:text-4xl" href="https://drive.google.com/drive/folders/1jFVjXXfh2_6QPp0lSBHFNT0OfNvbeJE6?usp=sharing" target=”_blank” > 2023-2024 Lessons</a>
+</div>
+
+<iframe class="w-[60%] mx-[20%] m-8 sm:h-[30vw]" src="https://docs.google.com/presentation/d/e/2PACX-1vQmrQZ48xUAPrZEpC6BdzTsrgaZYxyTAmSQ6GC6ivybRqRHxsZ8vp811mN5ZNpEb152sLb8joGylSpX/embed?start=false&amp;loop=false&amp;delayms=30000"></iframe>
+
+
+
+<style lang="postcss">
+    .lesson{
+        frameborder:0;
+        width:50%;
+        margin: 20px;
+        margin-left: 25%;
+        margin-right: 25%;
+        height:30vw;
+    }
+
+    .link{
+        cursor: pointer;
+        -webkit-transition-property: all; 
+        -webkit-transition-duration: 0.3s; 
+        -webkit-transition-timing-function: ease; 
+    }
+</style>
