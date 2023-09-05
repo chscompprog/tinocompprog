09ca980deb82c06e3ee766fa3461cc9e069b8ed3
richardz403 <116985684+richardz403@users.noreply.github.com>
2023-09-04 19:33:11 -0700

    home page

--- tcpc/src/routes/+page.svelte
+++ tcpc/src/routes/+page.svelte
@@ -1,268 +1,88 @@
-<div style="margin-top: 120px" />
+<script>
+	import tcpc from '$lib/tcpc2.jpg';
+	import big from '$lib/bigtcpc.jpg';
+	/**
+	 * @type {any}
+	 */
+	let screenSize;
+</script>
 
-<div class="ms-4 mt-4">
-	<h2 class="text-center text-6xl font-bold text-white drop-shadow-lg">Resources</h2>
-	<br />
+<svelte:window bind:innerWidth={screenSize} />
 
-	<h4 class="text-center text-2xl font-bold text-white drop-shadow-lg">USACO</h4>
-	<ul class="text-center">
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="http://www.usaco.org/">Official Site (usaco.org)</a>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://usaco.guide/">USACO Guide</a>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a
-				target="_blank"
-				href="https://docs.google.com/spreadsheets/d/1z2k-fv6q9KPHDuZu1arBvwIuKZ4HCoBRfHQR0UryPMQ/edit#gid=834662694"
-				>Problems Spreadsheet</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://codeforces.com/blog/entry/78158"
-				>Tracker Spreadsheets (by level)</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.overleaf.com/project/5f066f5dfb993800018cb314"
-				>Unofficial USACO Syllabus</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a
-				target="_blank"
-				href="https://www.reddit.com/r/usaco/comments/pk3tjp/the_ultimate_usaco_practice_method/"
-				>Practice Method Guide</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://ioinformatics.org/page/syllabus/12">IOI Syllabus</a>
-		</li>
-	</ul>
+<div style="position:relative;text-align:center;">
+	{#if screenSize < 768}
+		<img
+			src={big}
+			alt="tcpc"
+			class="w-[99%] mx-[0.5%]"
+			style="opacity:1;margin-top:100px;filter: blur(0.2vw); border-radius:5px;border-width:3px"
+		/>
+		<h1
+			class=" drop-shadow-lg"
+			style="position:absolute;top:50%;left:50%;transform:translate(-50%, -50%);font-size:13vw;font-weight: bold;color:white;"
+		>
+			Tino Competitive Programming
+		</h1>
+	{:else}
+		<img
+			src={tcpc}
+			alt="tcpc"
+			class="w-[96%] mx-[2%]"
+			style="opacity:1;margin-top:120px;filter: blur(0.2vw);border-radius:5px;border-width:3px"
+		/>
+		<h1
+			class=" drop-shadow-lg"
+			style="position:absolute;top:50%;left:50%;transform:translate(-50%, -50%);font-size:8vw;font-weight: bold;color:white;"
+		>
+			Tino Competitive Programming
+		</h1>
+	{/if}
+</div>
+<h1 class=" text-[4vw] drop-shadow-lg text-center text-white font-semibold">
+	Fridays at Lunch in Room 314
+</h1>
 
-	<h4 class="text-center text-2xl font-bold text-white drop-shadow-lg">Other Contests</h4>
-	<ul class="text-center">
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://codeforces.com/">Codeforces</a>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://atcoder.jp/?lang=en">AtCoder</a>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.codechef.com/">CodeChef</a>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://dmoj.ca/">DMOJ</a>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://codingcompetitions.withgoogle.com/"
-				>Google's Coding Competitions</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.facebook.com/codingcompetitions/hacker-cup/"
-				>Meta Hacker Cup</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a
-				target="_blank"
-				href="https://www.topcoder.com/thrive/tracks?track=Competitive%20Programming">Topcoder</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://clist.by/">Contest Calendar (has non-CP comps as well)</a>
-		</li>
-	</ul>
-
-	<h4 class="text-center text-2xl font-bold text-white drop-shadow-lg">Algorithms</h4>
-	<ul class="text-center">
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://visualgo.net/en">Algorithms Visualized</a>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://codeforces.com/edu/courses">Codeforces EDU</a>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://cp-algorithms.com/">CP Algorithms</a>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://github.com/kth-competitive-programming/kactl">KACTL</a>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://judge.yosupo.jp/">Library Checker</a>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a
-				target="_blank"
-				href="https://docs.google.com/document/d/1_dc3Ifg7Gg1LxhiqMMmE9UbTsXpdRiYh4pKILYG2eA4/edit"
-				>User-made ICPC Syllabus</a
-			>
-		</li>
-	</ul>
-
-	<h4 class="text-center text-2xl font-bold text-white drop-shadow-lg">Books</h4>
-	<ul class="text-center">
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://cses.fi/book/book.pdf">Competitive Programmer's Handbook</a>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a
-				target="_blank"
-				href="https://github.com/prasadgujar/CompetitiveProgramming/blob/master/book/Competitive%20Programming%203.pdf"
-				>Competitive Programming 3</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.csc.kth.se/~jsannemo/slask/main.pdf"
-				>Principles of Algorithmic Problem Solving</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://comscigate.com/Books/contests/icpc.pdf"
-				>The Hitchhiker's Guide to the Programming Contests</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://darrenyao.com/usacobook/java.pdf"
-				>An Introduction to the USA Computing Olympiad (Java)</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://darrenyao.com/usacobook/cpp.pdf"
-				>An Introduction to the USA Computing Olympiad (C++)</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://dp-book.com/Dynamic_Programming.pdf"
-				>Dynamic Programming for Computing Contests</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.dropbox.com/s/z2lur71042pjaet/guide.pdf?dl=0"
-				>Crash Course Coding Companion</a
-			>
-		</li>
-	</ul>
-
-	<h4 class="text-center text-2xl font-bold text-white drop-shadow-lg">Youtube Channels</h4>
-	<ul class="text-center">
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UCVp5MlM5aQbhObh5JYlgHUw"
-				>Algorithms Conquered</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UCBLr7ISa_YDy5qeATupf26w"
-				>Algorithms Live!</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UCSQmpb_K_I37JN-QLArZbFw"
-				>benritmicocode</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UCJtyODN6zjWnl4lwLHwfdcw"
-				>Codeforces With Java</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UCpvS3EykHW--l0ogUhMEjEw"
-				>Colin Galen</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UC0PYAh68WLY69tMXCzPKpLA">Dardev</a>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UCBr_Fu6q9iHYQCh13jmpbrg">Errichto</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UCuWLIm0l4sDpEe28t41WITA"
-				>Jonathan Paulson</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/user/luisoncpp">luis enrique vargas azcona</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UCe14db9xzTx8DNPUPxNKO9g"
-				>Code Tree (in Korean)</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/c/mitocw">MIT OpenCourseWare</a>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UC05LugecRCvKLip8Tqz2krA"
-				>Niema Moshiri</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UCXbCohpE9IoVQUD2Ifg1d1g"
-				>SecondThread</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UCV2g02zq5y7unJ_GSr-de2w"
-				>Stable Sort</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UCyTPeByJ_FvAJljtc0svt-Q"
-				>Stefan Dascalescu</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UCdNNY8Y8meG3z9Wy6MTzcLg"
-				>Riya Arora</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/channel/UCKuDLsO0Wwef53qdHPjbU2Q"
-				>William Lin</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="https://www.youtube.com/user/purpongie">WilliamFiset</a>
-		</li>
-	</ul>
-
-	<h4 class="text-center text-2xl font-bold text-white drop-shadow-lg">Other</h4>
-	<ul class="text-center">
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a
-				target="_blank"
-				href="https://docs.google.com/spreadsheets/d/1-n9Fnvhsnvsqh-IerE_yyIshw5RUNer_7EjwF_GW-TA/edit#gid=0"
-				>Mostafa Public Problem Lists</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a
-				target="_blank"
-				href="https://docs.google.com/spreadsheets/d/1iJZWP2nS_OB3kCTjq8L6TrJJ4o-5lhxDOyTaocSYc-k/edit#gid=84654839"
-				>Junior Training Sheet</a
-			>
-		</li>
-		<li class="text-center easeAni m-4 font-semibold inline-block text-white drop-shadow-md">
-			<a target="_blank" href="http://oichecklist.pythonanywhere.com/">OI Problem Checklist</a>
-		</li>
-	</ul>
+<div class="infoBlock my-[50px] bg-purple-200 shadow-lg">
+	<h1 class=" text-5xl font-bold m-5">About Us</h1>
+	<p class="sm:px-10 m-5 text-lg font-semibold">
+		Tino Competitive Programming is a club centered around hands-on problem solving. Throughout the
+		year, club members will learn various algorithms and practice implementing them by solving real
+		competitive programming problems. With the help of our qualified mentors, club members will
+		prepare for coding contests such as USACO (USA Computing Olympiad) and other prestigious online
+		competitions. Join us and boost your critical thinking and coding skills!
+	</p>
 </div>
 
-<style>
-	.easeAni {
+<div class="infoBlock my-[50px] bg-purple-200 shadow-lg">
+	<h1 class=" text-5xl font-bold m-5">Why Join</h1>
+	<p class="sm:px-10 m-5 text-lg font-semibold">
+		Joining Tino Competitive Programming is a great oppurtunity that brings many benefits. Our club
+		helps prepare students for school courses such as Intro to Java and APCS, as well as learning
+		beyond the school's scope. Competitive Programming is a great was to boost college applications
+		and gain skills for coding interviews. You will be able to greatly improve your programming
+		speed, knowledge of useful algorithms and data structures, logical thinking and problem solving.
+		It will teach you a lot about programming in general and will put you on a path towards success
+		in your future CS or STEM endeavors.
+	</p>
+</div>
+
+<style lang="postcss">
+	.infoBlock {
+		width: 80%;
+		margin-left: 10%;
+		margin-right: 10%;
+		height: auto;
+		opacity: 1;
+		border-radius: 20px;
+		padding: 10px;
+		text-align: center;
 		cursor: pointer;
 		-webkit-transition-property: all;
 		-webkit-transition-duration: 0.3s;
 		-webkit-transition-timing-function: ease;
 	}
 
-	.easeAni:hover {
+	.infoBlock:hover {
 		transform: scale(1.1);
 	}
 </style>
